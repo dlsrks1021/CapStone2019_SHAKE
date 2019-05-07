71a7bf59eba23bfa2c0f179a56d0b8c30687977f
unknown <dlsrks1021@gmail.com>
2019-05-07 17:47:35 +0900

    reviewActivity_phpconnect not yet

--- Android/app/src/main/java/com/example/user/shake/ReviewActivity.java
+++ Android/app/src/main/java/com/example/user/shake/ReviewActivity.java
@@ -1,38 +1,70 @@
 package com.example.user.shake;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RatingBar;
+import android.widget.Toast;
+
+import java.util.concurrent.ExecutionException;
 
 public class ReviewActivity extends AppCompatActivity {
 
     RatingBar ratingBar;
     EditText review;
-    Button returnButton;
+    Button submitButton;
+
+    float rating;
+    String userId;
+    int rentnumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_review);
 
+        Intent intent = getIntent();
+        userId = intent.getStringExtra("userId");
+        rentnumber = intent.getIntExtra("rentnumber", -1);
+
         ratingBar = findViewById(R.id.reviewRatingBar);
         review = findViewById(R.id.reviewEdit);
-        returnButton = findViewById(R.id.reviewReturnButton);
+        submitButton = findViewById(R.id.reviewSubmitButton);
+
+        rating = (float) 3.5;
 
         ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
             @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean b) {
-
+            public void onRatingChanged(RatingBar ratingBar, float rating_val, boolean b) {
+                rating = rating_val;
             }
         });
 
-        returnButton.setOnClickListener(new View.OnClickListener() {
+        submitButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                if (rentnumber == -1){
+                    Toast.makeText(getApplicationContext(),"rentnumber error!",Toast.LENGTH_SHORT).show();
+                }
+                if (review.getText().toString().equals("")){
+                    Toast.makeText(getApplicationContext(),"내용을 입력해주세요!",Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    PhpConnect task = new PhpConnect();
+                    try {
+                        task.execute("http://13.125.229.179/insertReview.php?rentnumber="+rentnumber+"&contents="+review.getText().toString()+"&imageUrl=").get();
+                        Toast.makeText(getApplicationContext(),"리뷰가 등록되었습니다!",Toast.LENGTH_SHORT).show();
+                        finish();
+                    }catch (InterruptedException e){
+                        e.printStackTrace();
+                    }catch (ExecutionException e) {
+                        e.printStackTrace();
+                    }
+                    finish();
+                }
             }
         });
 
