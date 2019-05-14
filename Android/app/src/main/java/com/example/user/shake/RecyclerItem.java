8e8ebbf49c47bfe96891ec764885b5aaff8141bc
unknown <dlsrks1021@gmail.com>
2019-05-14 16:52:54 +0900

    reviewList_save file

--- Android/app/src/main/java/com/example/user/shake/RecyclerItem.java
+++ Android/app/src/main/java/com/example/user/shake/RecyclerItem.java
@@ -4,7 +4,7 @@ import java.util.Date;
 
 public class RecyclerItem{
 
-    private String name, owner, bike;
+    private String name, owner, bike, content;
     private float rating;
     private int rentTime;
     private Date date;
@@ -15,5 +15,29 @@ public class RecyclerItem{
 
     public String getName() {
         return name;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public int getRentTime() {
+        return rentTime;
+    }
+
+    public String getBike() {
+        return bike;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public String getContent() {
+        return content;
     }
 }
--- Android/app/src/main/java/com/example/user/shake/ReviewListActivity.java
+++ Android/app/src/main/java/com/example/user/shake/ReviewListActivity.java
@@ -4,8 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.concurrent.ExecutionException;
 
 public class ReviewListActivity extends AppCompatActivity {
 
@@ -19,7 +21,6 @@ public class ReviewListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_review_list);
         recyclerView = findViewById(R.id.recyclerView);
         setRecyclerView();
-
     }
 
     private void setRecyclerView(){
@@ -31,8 +32,15 @@ public class ReviewListActivity extends AppCompatActivity {
 
     private void setData(){
         mItems.clear();
-        for(String name : names){
-            mItems.add(new RecyclerItem(name));
+
+        PhpConnect task = new PhpConnect();
+        try {
+            task.execute("http://13.125.229.179/insertReview.php?havetoimplement").get();
+
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }catch (ExecutionException e) {
+            e.printStackTrace();
         }
     }
 
